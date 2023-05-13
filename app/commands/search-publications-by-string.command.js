d55b8ea97868bb4030b4964f13bf59c6e7d26736
Shahnawaz Akhtar <sakhtar0092@gmail.com>
2023-05-13 15:13:26 +0100

    aaa

--- app/commands/search-publications-by-string.command.js
+++ app/commands/search-publications-by-string.command.js
@@ -2,15 +2,40 @@ export const semantic_search_post_table = {
     name: "semantic_search_post_table",
     args: "{search_term: str ,  extract_variables: list}",
     inputData: "",
-    description: "This function takes a search string and a list of variables that need to be extracted for given search terms. It uses embeddings database to do the search and returns the rows that have high similarity wrt search term. Finally it keeps only the unique values for variables that we need to extract.",
+    description: "This function takes a search string and a list of variables that need to be extracted for given search terms. It uses embeddings database to do the search and returns the rows that have high similarity wrt search term. Finally it keeps only the unique values for variables that we need to extract. The extract_variables can only take one of the following values ['content','post_id','profile_id','main_content_focus','language','block_timestamp','content_uri']",
     outputDescription: "List unique values of the variables to extract based on our semantic search criteria"
 };
 
-
-export const semantic_search_post_table = {
-    name: "semantic_search_post_table",
-    args: "{search_term: str ,  extract_variables: list}",
+export const get_information_from_lens = {
+    name: "get_information_from_lens",
+    args: "{query_string: str}",
     inputData: "",
-    description: "This function takes a search string and a list of variables that need to be extracted for given search terms. It uses embeddings database to do the search and returns the rows that have high similarity wrt search term. Finally it keeps only the unique values for variables that we need to extract.",
-    outputDescription: "List unique values of the variables to extract based on our semantic search criteria"
+    description: "This function takes a query string and asks chatGPT to create a GraphQL query based on human language query. After that, it executes that query to fetch results from lens protocol.",
+    outputDescription: "JSON object respresenting information resquested"
 };
+
+export const get_profile_information_from_lens = {
+    name: "get_profile_information_from_lens",
+    args: "{profile_id: str, information_required}",
+    inputData: "",
+    description: "This function takes a lens profile ID or address, and information required and then gets information required from all the information available about that profile on the lens protocol.",
+    outputDescription: "JSON object respresenting profile information"
+};
+
+export const get_profile_information_from_airstack = {
+    name: "get_profile_information_from_airstack",
+    args: "{profile_id: str, information_required}",
+    inputData: "",
+    description: "This function takes a lens profile ID or address , and information required and then gets information required from all the information available about that profile on the airstack.",
+    outputDescription: "JSON object respresenting profile information"
+};
+
+export const get_profile_information_from_the_graph = {
+    name: "get_profile_information_from_the_graph",
+    args: "{profile_id: str, information_required}",
+    inputData: "",
+    description: "This function takes a lens profile ID or address, and information required and then gets information required from all the information available about that profile on the the graph protocol.",
+    outputDescription: "JSON object respresenting profile information"
+};
+
+
--- function_signatures.py
+++ function_signatures.py
@@ -1,44 +1,4 @@
 
-
-post_table = ['content','profile_id','post_id']
-
-
-
-def semantic_search_post_table(search_term: str ,  extract_variables: list):
-    # Search Embeddings Database
-    # Take top N results and get the variables requested in extract_variables
-    return list
-
-
-def get_followers_information(profile_id):
-    # Get all the followers of this given profile_id
-    # Get information about those profiles
-    return dict
-
-
-def get_profile_information_from_lens(profile_id: str, information_required):
-def get_profile_information_from_lens(profileHandle: str, information_required):
-    return dict
-
-def get_profile_information_from_airstack(profile_id: str, information_required):
-    return dict
-
-def get_profile_information_from_thegraphql(profile_id: str, information_required):
-    return dict
-# def get_profile_information_from_thegraphql(graphql_query: str, information_required):
-#     return dict
-
-
-def summarize_profile(profile_id):
-    # Ask GPT to summarize the content
-    return
-
-
-def find_similarity_between_profiles(profile_id_1, profile_id_2):
-    return None
-
-def aggregate_profile_information([profile_information1,]):
-
 # Find me people talking about longevity:  Block 1
 # Who are my weekly active followrs
 # Weekly followers
