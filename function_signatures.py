402a1e1a11e6279f4355cbf52765dbee8a2fb505
Alexander Kozlov <itrefak@gmail.com>
2023-05-13 15:46:16 +0100

    merge

---
+++ function_signatures.py
@@ -0,0 +1,20 @@
+
+# Find me people talking about longevity:  Block 1
+# Who are my weekly active followrs
+# Weekly followers
+
+# Who are not collecting my posts.
+# Who collected more that one post
+# WHo coleected more than 5 mosts
+# Top 10 most engaging followers
+# Weekly followers who are not collecting my posts.
+
+# Find publication (by author, by keyword, by topic)
+# Find people that follow me, that talk about longevity
+# Find find people in domain data science that I follow
+# Find posts that talk about longevity and have more that 2 replies and 1 like
+# Find people that write about solidity developement
+# Find people writing about defi and have more than 500 followers
+# Give a quick summary about a person based on lens handle.
+# Find top 10 investors by followerswho talk about network state
+# Find all posts that contain video and talk about space tech.
