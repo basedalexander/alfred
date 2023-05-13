d3f0eb0f298254e866f27368f0bc24a05df73a2a
Alexander Kozlov <itrefak@gmail.com>
2023-05-13 09:16:02 +0100

    bot

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# alfred
+#Personal network assistant Alfred
+
+Key features:
+* Communicate in a natural language via telegram bot.
+* Get insights from your network graph or from Lens network in general
+
+Examples:
+- Show me the most influential people that talk about DAOs.
+- Is there anyone in my followers posting about fashion? Show a tldr for each person
---
+++ tg-bot/main.js
@@ -0,0 +1,52 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const express = require('express');
+const app = express();
+app.use(express.urlencoded({extended:true}));
+
+let TELEGRAM_BOT_TOKEN = process.env.TELEGRAM_BOT_TOKEN;
+
+if (process.env.mode || process.env.MODE === 'dev') {
+  const devConfig = require('../config.dev.json');
+  TELEGRAM_BOT_TOKEN = devConfig.TELEGRAM_BOT_TOKEN;
+}
+
+const bot = new TelegramBot(TELEGRAM_BOT_TOKEN, {polling: true});
+
+bot.on('message', async (msg) => {
+  try {
+    await handleTelegramMessage(msg);
+  } catch (e) {
+    replyError(msg.chat.id, e);
+    return;
+  }
+});
+
+async function handleTelegramMessage (msg) {
+  const chatId = msg.chat.id;
+  console.log(`received message from chatid ${chatId}`);
+
+  const text = msg.text;
+  if (!text) {
+    bot.sendMessage(chatId, 'unsupported format from telegram');
+    return;
+  }
+
+  console.log(`chatId: ${chatId}, prompt: ${text}`);
+
+  let response = 'hello Bruce';
+
+  bot.sendMessage(chatId, `${response}`);
+}
+
+function replyError(chatId, err) {
+  bot.sendMessage(chatId, `error occured: ${err}`);
+}
+
+app.get('/ping', async (req, res) => {
+  res.send('pong');
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
