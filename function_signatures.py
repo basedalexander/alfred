2f716ca535e00da0d0fa2a7598913506ef4850d4
Shahnawaz Akhtar <sakhtar0092@gmail.com>
2023-05-13 13:04:43 +0100

    mm

---
+++ function_signatures.py
@@ -0,0 +1,54 @@
+
+
+post_table = ['content','profile_id','post_id']
+
+def semantic_search_post_table(search_term: str ,  extract_variables: list):
+    # Search Embeddings Database
+    # Take top N results and get the variables requested in extract_variables
+    return list
+
+
+def get_followers_information(profile_id):
+    # Get all the followers of this given profile_id
+    # Get information about those profiles
+    return dict
+
+
+def get_profile_information_from_lens(graphql_query: str, information_required):
+    return dict
+
+def get_profile_information_from_airstack(graphql_query: str, information_required):
+    return dict
+
+def get_profile_information_from_thegraphql(graphql_query: str, information_required):
+    return dict
+
+def summarize_profile(profile_id):
+    # Ask GPT to summarize the content
+    return
+
+
+def find_similarity_between_profiles(profile_id_1, profile_id_2):
+    return None
+
+def aggregate_profile_information([profile_information1,]):
+
+# Find me people talking about longevity:  Block 1
+# Who are my weekly active followrs
+# Weekly followers
+
+# Who are not collecting my posts.
+# Who collected more that one post
+# WHo coleected more than 5 mosts
+# Top 10 most engaging followers
+# Weekly followers who are not collecting my posts.
+
+# Find publication (by author, by keyword, by topic)
+# Find people that follow me, that talk about longevity
+# Find find people in domain data science that I follow
+# Find posts that talk about longevity and have more that 2 replies and 1 like
+# Find people that write about solidity developement
+# Find people writing about defi and have more than 500 followers
+# Give a quick summary about a person based on lens handle.
+# Find top 10 investors by followerswho talk about network state
+# Find all posts that contain video and talk about space tech.
