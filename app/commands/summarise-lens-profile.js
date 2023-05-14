568d3be20b9d24e1f8598f0e8ea7d2797cf114aa
Daniel Duma <danielduma@gmail.com>
2023-05-14 06:45:47 +0100

    push

--- app/commands/summarise-lens-profile.js
+++ app/commands/summarise-lens-profile.js
@@ -30,29 +30,25 @@ function collectPosts(posts) {
 }
 
 async function summariseLensProfile(args) {
-
-    console.log(args);
     // 1. Get lens profile by id
     const lensProfile = await profileDatasource.getByHandle(args.profileHandle);  
 
     let prof = lensProfile.data.profile;
-    // console.log(prof);
-
 
     // 2. Get all posts by lens profile id
     const posts = await publicationsDatasource.getByIdsGraphQL(prof.id);
 
     let post_text = collectPosts(posts);
-    console.log(post_text);
+
 
     // 3. Generate prompt based on posts
     const prompt = generateSummaryPrompt(post_text, lensProfile);
     const result = await openai.ask(prompt);
-    console.log(result);
+    // console.log(result);
     // 4. Return summarised posts
 
 }
 
-summariseLensProfile({ "profileHandle": "stani.lens" })
+// summariseLensProfile({ "profileHandle": "stani.lens" })
 
 module.exports = summariseLensProfile;
