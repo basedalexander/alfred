4d301254bdd510e6c4dade31c4f896400b800b43
Daniel Duma <danielduma@gmail.com>
2023-05-14 03:51:51 +0100

    fixfes

--- app/commands/get-posts-by-content-type.js
+++ app/commands/get-posts-by-content-type.js
@@ -1,4 +1,25 @@
+const Publication = require('../datasources/publications-datasource');
+const pub = new Publication();
+
+const POST_TEMPLATE = `**{title}**`.format(title)
+
+function createPostTemplate(publication) {
+    const template = `
+        ${publication.page_content}
+        by ${publication.profile_id} at ${publication.block_timestamp}
+    `;
+    return template;
+}
+
 async function getPostsByContentAndType(args) {
+    let pubs = await pub.getByQuery(args);
+    let result = [];
+    for (let i = 0; i < pubs.length; i++) {
+        result.push(createPostTemplate(pubs[i]));
+    }
+    // join all posts
+    result = result.join('\n\n');
+    return result;
 }
 
 module.exports = getPostsByContentAndType;
--- app/commands/summarise-lens-profile.js
+++ app/commands/summarise-lens-profile.js
@@ -1,4 +1,19 @@
 async function summariseLensProfile(args) {
+
+    // 1. Get lens profile by id
+    const lensProfile = await lensProfileDatasource.getById(args);  
+
+
+
+    // 2. Get all posts by lens profile id
+    const posts = await postsDatasource.getByLensProfileId(args);
+    
+
+    // 3. Summarise all posts
+    const summarisedPosts = summarisePosts(posts);
+    
+    // 4. Return summarised posts
+
 }
 
 module.exports = summariseLensProfile;
